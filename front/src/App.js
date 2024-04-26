8f4d9ea953afae5b015809bad71f9da4903733f9
hana0627 <hanana9506@naver.com>
2024-04-26 11:42:29 +0900

    #4 로그인페이지 만들기
    제법 예쁘게 만들어졌다.
    로그인 버튼 클릭시 서버로 요청이 나가는것을 확인하였음

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import {Route, Routes} from 'react-router-dom'
 import LoginPage from "./pages/LoginPage";
+
 function App() {
   return (
       <div className="App">
+          <link rel="stylesheet" href="/css/style.css" />
           <Routes>
               <Route path='/login' element={<LoginPage/>} />
               <Route path='/*' element={<div>없는페이지에옹</div>} />
--- front/src/pages/LoginPage.jsx
+++ front/src/pages/LoginPage.jsx
@@ -1,21 +1,45 @@
 import axios from 'axios';
+import React, { useState } from 'react'
 
 function LoginPage() {
 
+    const [memberId, setMemberId] = useState('')
+    const [password, setPassword] = useState('')
+
+    function memberIdHandle(e) {
+        setMemberId(e.target.value)
+    }
+    function passwordHandle(e) {
+        setPassword(e.target.value)
+    }
+
     function loginBtnClick() {
-        axios.get('http://localhost:8080/hello')
+
+        axios.post('http://localhost:8080/hello',
+            {
+                "memberId": memberId,
+                "password": password
+            }
+            )
             .then((response) => {
                 console.log(response.data);
             })
             .catch(() => {
-                console.error('에러발생');}
-        )
+                    console.error('에러발생');
+                }
+            )
     }
 
     return (
-        <button onClick={()=> loginBtnClick()}>누르고 싶게 생긴 버튼</button>
+        <>
+            <div className="login-wrapper">
+                <h2>Login</h2>
+                    <input type="text" className = "login-input" placeholder="id" value={memberId} onChange={(e) => memberIdHandle(e)}/>
+                    <input type="password" className = "login-input" value={password} placeholder="Password"  onChange={(e) => passwordHandle(e)}/>
+                    <button className="btn_purple" onClick={loginBtnClick}>로그인</button>
+            </div>
+        </>
     )
 }
-
 
 export default LoginPage;
