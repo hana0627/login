138a4150614eddb114322d4c6bb44d446f0eb18f
hana0627 <hanana9506@naver.com>
2024-04-29 19:43:53 +0900

    #12 스프링시큐리티 코드변경 /api/v1/* -> /api/v1/** /api/v2/* -> /api/v2/** 로 하위경로까지 스프링시큐리티가 관리하게 변경하였다.

--- src/main/kotlin/com/hana/login/common/config/SecurityConfig.kt
+++ src/main/kotlin/com/hana/login/common/config/SecurityConfig.kt
@@ -18,8 +18,8 @@ class SecurityConfig {
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
         return http.csrf { csrf -> csrf.disable() }
             .authorizeHttpRequests { auth -> auth
-                .requestMatchers("/api/v1/*").permitAll()
-                .requestMatchers("/api/v2/*").authenticated()}
+                .requestMatchers("/api/v1/**").permitAll()
+                .requestMatchers("/api/v2/**").authenticated()}
             .formLogin { form -> form.disable() }
             .httpBasic{ b -> b.disable()}
             .build()
