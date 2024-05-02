b2d7f94176ced95de1dfb183f2b6326277abe7ab
hana0627 <hanana9506@naver.com>
2024-05-02 10:45:46 +0900

    #15 JWT토큰 백엔드 구성완료 추가적인 테스트코드 작성필요

--- src/main/kotlin/com/hana/login/common/config/filter/JwtFiler.kt
+++ src/main/kotlin/com/hana/login/common/config/filter/JwtFiler.kt
@@ -1,8 +1,6 @@
 package com.hana.login.common.config.filter
 
 import com.hana.login.common.utils.JwtUtils
-import com.hana.login.user.service.MemberService
-import io.jsonwebtoken.Claims
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
@@ -28,42 +26,48 @@ class JwtFiler(
         // permitAll으로 지정된 경로에 대해서는 jwt필터처리 수행x
         if (request.requestURL.contains("/api/v1")) {
             filterChain.doFilter(request, response)
-        } else {
-            // get Header
-            val header: String? = request.getHeader(HttpHeaders.AUTHORIZATION)
-
-            if(header == null || !header.startsWith("Bearer ")) {
-                log.info("Error occurred while getting AUTHORIZATION Header")
-                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "jwt 토큰 정보가 없습니다")
-                return
-            }
-
-            // "Bearer "이후의 문자열 추출
-            val token: String = header.split(" ")[1].trim()
-
-            if (secretKey == null) {
-                log.error("key is null")
-                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "secretKey가 존재하지 않습니다.")
-                return
-            }
-
-            // 토큰만료확인
-            if (jwtUtils.isExpired(token)) {
-                log.error("key is expired")
-                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "jwt 토큰이 만료되었습니다.")
-                filterChain.doFilter(request, response)
-            }
-
-
-            jwtUtils.decodeToken(token);
-
-            // 토큰 정보에서 memberId값 가져오기
-//            val memberId = jwtUtils.getMemberId(token)
-
-            // memberId를 SpringSecurity에 주입
-//            val authentication = UsernamePasswordAuthenticationToken(memberId, null, mutableListOf())
-//            SecurityContextHolder.getContext().authentication = authentication
         }
+
+        // get Header
+        val header: String? = request.getHeader(HttpHeaders.AUTHORIZATION)
+
+        if (header == null || !header.startsWith("Bearer ")) {
+            log.info("Error occurred while getting AUTHORIZATION Header")
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "jwt 토큰 정보가 없습니다")
+            return
+        }
+
+        // "Bearer "이후의 문자열 추출
+        val token: String = header.split(" ")[1].trim()
+
+        if (secretKey == null) {
+            log.error("key is null")
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "secretKey가 존재하지 않습니다.")
+            return
+        }
+
+
+        // 유효한 토큰인지 검증
+        if (jwtUtils.isInValidated(token)) {
+            log.error("inValidated Token")
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "토큰정보가 유효하지 않습니다.")
+            filterChain.doFilter(request, response)
+        }
+
+        // 토큰만료확인
+        if (jwtUtils.isExpired(token)) {
+            log.error("key is expired")
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "jwt 토큰이 만료되었습니다.")
+            filterChain.doFilter(request, response)
+        }
+
+        // 회원 아이디 추출
+        val memberId = jwtUtils.getMemberId(token)
+
+        val authentication = UsernamePasswordAuthenticationToken(memberId, null, mutableListOf())
+        SecurityContextHolder.getContext().authentication = authentication
+
+
         filterChain.doFilter(request, response)
     }
 }
--- src/main/kotlin/com/hana/login/common/utils/JwtUtils.kt
+++ src/main/kotlin/com/hana/login/common/utils/JwtUtils.kt
@@ -9,20 +9,21 @@ import org.springframework.stereotype.Component
 import java.nio.charset.StandardCharsets
 import java.security.Key
 import java.util.*
-import kotlin.collections.List
+import javax.crypto.Mac
+import javax.crypto.spec.SecretKeySpec
 
 @Component
-class JwtUtils(
-) {
-
+class JwtUtils() {
     @Value("\${jwt.secret-key}")
     private val secretKey: String? = null
+
     @Value("\${jwt.token.expired-time-ms}")
     private val expiredMs: Long? = null
 
 
-
-    // 토큰생성
+    /**
+     * 토큰생성
+     */
     fun generateToken(
         memberId: String,
         memberName: String,
@@ -38,29 +39,70 @@ class JwtUtils(
         return "Bearer " + Jwts.builder()
             .setClaims(claims)
             .setIssuedAt(Date(System.currentTimeMillis()))
-            .setExpiration(Date(System.currentTimeMillis() + expiredMs))
+            .setExpiration(Date(System.currentTimeMillis() + expiredMs * 1000))
             .signWith(getKey(secretKey), SignatureAlgorithm.HS256)
             .compact()
     }
 
-    // 토큰 만료여부 확인
+
+    /**
+     * 토큰 만료여부 확인
+     * 만료된 토큰 : return ture
+     * 만료되지 않은 토큰 : return false
+     */
     fun isExpired(token: String): Boolean {
         val expiredDate: Date = extreactClaims(token).expiration
         return expiredDate.before(Date())
     }
 
-    fun decodeToken(token: String): Boolean {
-        // 토큰을 각 섹션(Header, Payload, Signature)으로 분할
-        println("token = $token")
-        val chunks: List<String> = token.split(".")
-        val decoder = Base64.getUrlDecoder()
-        println("여기한번확인")
-        println("chunck.size() = ${chunks.size}")
-        chunks.forEach{it -> println(it) }
+    /**
+     * token -> 회원 아이디 추출
+     */
+    fun getMemberId(token: String): String {
+        val claims: Claims = extreactClaims(token)
+        return claims["memberId"].toString()
+    }
 
-        val header = String(decoder.decode(chunks[0]))
-        val payload = String(decoder.decode(chunks[1]))
-        return true
+    /**
+     *  유효하지 않은 토큰인지 검증
+     *  유요하지 않은 토큰 : return ture
+     *  유효한 토큰 : return false
+     */
+    fun isInValidated(token: String): Boolean {
+        // 토큰을 (Header, Payload, Signature)으로 분할
+        val chunks: List<String> = token.split(".")
+
+        // 시그니처 추출
+        val signature = chunks[2]
+
+        // 헤더+시그니처를 암호화
+        val headerAndClaims = "${chunks[0]}.${chunks[1]}"
+        val expectedSignature = generateSignature(headerAndClaims)
+
+        // 헤더+시그너처를 암호화한 값과 토큰의 Signature가 일치하는지 여부 확인
+        return signature != expectedSignature
+    }
+
+
+
+    private fun generateSignature(headerAndClaims: String): String {
+        if (secretKey == null || expiredMs == null) {
+            throw NullPointerException("key 혹은 expiredMs가 존재하지 않습니다.")
+        }
+
+        // 시크릿 키를 바이트 배열로 변환
+        val keyBytes = secretKey.toByteArray(Charsets.UTF_8)
+
+        // HMAC-SHA256 알고리즘을 사용하여 Mac 객체 초기화
+        val hmacSha256 = Mac.getInstance("HmacSHA256")
+        val secretKeySpec = SecretKeySpec(keyBytes, "HmacSHA256")
+        hmacSha256.init(secretKeySpec)
+
+        // 입력 값을 사용하여 서명 생성
+        val signatureBytes = hmacSha256.doFinal(headerAndClaims.toByteArray(Charsets.UTF_8))
+
+        // Base64 인코딩하여 문자열로 반환 (패딩 없는 Base64 인코딩 사용)
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(signatureBytes)
     }
 
 
@@ -74,6 +116,7 @@ class JwtUtils(
             .build().parseClaimsJws(token).body
     }
 
+    // 서명키 생성
     private fun getKey(key: String): Key {
         val keyByte: ByteArray = key.toByteArray(StandardCharsets.UTF_8)
         return Keys.hmacShaKeyFor(keyByte)
