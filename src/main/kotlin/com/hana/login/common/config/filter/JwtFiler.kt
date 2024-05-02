a381197c2b0e55ed20d782bcb822eee1b97abebc
hana0627 <hanana9506@naver.com>
2024-05-02 11:33:23 +0900

    #15 [BUGFIX] else 분기 추가
    프론트엔드 통신중에 의도하지 않은 스크립트에러가 발생하여 else 구문으로 포장하였음

--- src/main/kotlin/com/hana/login/common/config/filter/JwtFiler.kt
+++ src/main/kotlin/com/hana/login/common/config/filter/JwtFiler.kt
@@ -27,47 +27,43 @@ class JwtFiler(
         if (request.requestURL.contains("/api/v1")) {
             filterChain.doFilter(request, response)
         }
+        else {
+            // get Header
+            val header: String? = request.getHeader(HttpHeaders.AUTHORIZATION)
 
-        // get Header
-        val header: String? = request.getHeader(HttpHeaders.AUTHORIZATION)
+            if (header == null || !header.startsWith("Bearer ")) {
+                log.info("Error occurred while getting AUTHORIZATION Header")
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "jwt 토큰 정보가 없습니다")
+                return
+            }
 
-        if (header == null || !header.startsWith("Bearer ")) {
-            log.info("Error occurred while getting AUTHORIZATION Header")
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "jwt 토큰 정보가 없습니다")
-            return
-        }
+            // "Bearer "이후의 문자열 추출
+            val token: String = header.split(" ")[1].trim()
 
-        // "Bearer "이후의 문자열 추출
-        val token: String = header.split(" ")[1].trim()
+            if (secretKey == null) {
+                log.error("key is null")
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "secretKey가 존재하지 않습니다.")
+                return
+            }
+            // 유효한 토큰인지 검증
+            if (jwtUtils.isInValidated(token)) {
+                log.error("inValidated Token")
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "토큰정보가 유효하지 않습니다.")
+                filterChain.doFilter(request, response)
+            }
+            // 토큰만료확인
+            if (jwtUtils.isExpired(token)) {
+                log.error("key is expired")
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "jwt 토큰이 만료되었습니다.")
+                filterChain.doFilter(request, response)
+            }
+            // 회원 아이디 추출
+            val memberId = jwtUtils.getMemberId(token)
 
-        if (secretKey == null) {
-            log.error("key is null")
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "secretKey가 존재하지 않습니다.")
-            return
-        }
+            val authentication = UsernamePasswordAuthenticationToken(memberId, null, mutableListOf())
+            SecurityContextHolder.getContext().authentication = authentication
 
-
-        // 유효한 토큰인지 검증
-        if (jwtUtils.isInValidated(token)) {
-            log.error("inValidated Token")
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "토큰정보가 유효하지 않습니다.")
             filterChain.doFilter(request, response)
         }
-
-        // 토큰만료확인
-        if (jwtUtils.isExpired(token)) {
-            log.error("key is expired")
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "jwt 토큰이 만료되었습니다.")
-            filterChain.doFilter(request, response)
-        }
-
-        // 회원 아이디 추출
-        val memberId = jwtUtils.getMemberId(token)
-
-        val authentication = UsernamePasswordAuthenticationToken(memberId, null, mutableListOf())
-        SecurityContextHolder.getContext().authentication = authentication
-
-
-        filterChain.doFilter(request, response)
     }
 }
