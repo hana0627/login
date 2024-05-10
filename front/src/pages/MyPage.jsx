b9ec79a3d40e18f4f7e8d74a3bd486651da81557
hana0627 <hanana9506@naver.com>
2024-05-10 13:17:56 +0900

    #32 로그아웃 프론트엔드 기능 구현

--- front/src/pages/MyPage.jsx
+++ front/src/pages/MyPage.jsx
@@ -1,14 +1,15 @@
 import React, {useEffect, useState} from 'react'
 import axios from "axios";
 import Swal from "sweetalert2";
+import {useNavigate} from "react-router-dom";
 
 function MyPage() {
-    
+
     const [isRender, setIsRender] = useState(false)
     const [userId, setUserId] = useState(false)
     const [userName, setUserName] = useState(false)
     const [phoneNumber, setPhoneNumber] = useState(false)
-
+    const navigate = useNavigate()
 
     const accessToken = localStorage.getItem('accessToken')
     function getData() {
@@ -44,7 +45,39 @@ function MyPage() {
     }
 
     function logOutBtnClick() {
-        alert("기능 개발중입니다.")
+        axios.get('http://localhost:8080/api/v2/logout',{headers: {Authorization: accessToken}})
+            .then(response => {
+                localStorage.removeItem("accessToken")
+                Swal.fire({
+                    title: '안내',
+                    html: '로그아웃 되었습니다',
+                    icon: 'warning',
+                    confirmButtonText: '확인'
+                }).then(response => {
+                    if (response.isConfirmed) {
+                        navigate('/login');
+                    }
+                });
+            })
+            .catch(error => {
+                const data = error.response.data
+                if(data.getCode === 'INTERNAL_SERVER_ERROR') {
+                    Swal.fire({
+                        title: '실패',
+                        html: '예상하지 못한 에러가 발생했습니다.<br>다시 시도해주세요',
+                        icon: 'warning',
+                        confirmButtonText: '확인'
+                    });
+                }
+                else {
+                    Swal.fire({
+                        title: '실패',
+                        html: data.getMessage,
+                        icon: 'warning',
+                        confirmButtonText: '확인'
+                    });
+                }
+            })
     }
 
 
